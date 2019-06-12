17436273a270090956f727790c0217c46fb74ae4
Dominick <dominick@finnest.co>
2019-06-12 16:31:58 -0400

    Initial Commit

---
+++ domStar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  domStar
+//
+//  Created by Dominick Hera on 6/12/19.
+//  Copyright © 2019 Dominick Hera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
